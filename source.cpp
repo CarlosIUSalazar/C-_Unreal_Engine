270cb8ffb65a1dfb2fcb392fa1651cd1268f7c05
Carlos Salazar <carlosiusalazar@gmail.com>
2023-09-22 22:09:19 +0900

    lesson 4

--- source.cpp
+++ source.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+using namespace std; //we want to use standard namespace (such as cout) so it doesnt have to be prefaces by std::
 
 int main() {
-    std::cout << "You died!\n";
+    char myCharacter;
+    myCharacter = 'y'; //'' is for single characters "" is for strings
+
+    int myInt;
+    myInt = 13; 
+    
+    cout << myCharacter << endl; //endl also belongs to std namespace
+    cout << myInt << endl;
+
+    myCharacter = 'n';
+    myInt = 12;
+
+    cout << myCharacter << endl; //endl also belongs to std namespace
+    cout << myInt << endl;
+
     //system("pause");
-    std::cin.get();
+    cin.get();
 
 }
